cee46ceeaa9ecbf5a5b6b3b295aa877bde782558
Daniel Kolář <daniel1142@seznam.cz>
2022-12-13 00:26:07 +0100

    Přidejte soubory projektu.

---
+++ Souřadnice/Program.cs
@@ -0,0 +1,5 @@
+﻿using Souřadnice;
+
+Coordinates prvni = new Coordinates(5, 6);
+Console.WriteLine(prvni.Equals(new Coordinates(5, 6)));
+Console.WriteLine(prvni.CompareTo(new Coordinates(11, 9)));
---
+++ Souřadnice/Souřadnice.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Souřadnice
+{
+    internal class Coordinates : IEquatable<Coordinates>, IComparable<Coordinates>
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Coordinates(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public bool Equals(Coordinates other)
+        {
+            if (other == null) return false;
+            return (X == other.X && Y == other.Y);
+        }
+
+        public int CompareTo(Coordinates other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+
+            if (X != other.X)
+            {
+                return X.CompareTo(other.X);
+            }
+            else
+            {
+                return Y.CompareTo(other.Y);
+            }
+        }
+
+
+    }
+}
